98b3d920f05a3d0a15b1a6b85de116416a52ec29
Gunel99 <45911420+Gunel99@users.noreply.github.com>
2021-05-13 01:12:56 +0400

    Basket added

--- 12.05.21/assets/js/script.js
+++ 12.05.21/assets/js/script.js
@@ -7,7 +7,6 @@ fetch(`https://isko88.github.io/apipizza.json`)
 })
 .then(products => {
     products.forEach(product => {
-        console.log(product);
         let col = document.createElement('div');
         col.className = "col-3 p-2";
         let card = document.createElement('div');
@@ -20,6 +19,10 @@ fetch(`https://isko88.github.io/apipizza.json`)
         cardImg.style.paddingTop = "25px";
         cardImg.src = product.image;
         cardImg.setAttribute('alt', 'Product image');
+        // let button = document.createElement('button');
+        // button.className = "btn btn-light";
+        // button.setAttribute("type", "button");
+        // button.textContent = "Add to basket";
         let cardBody = document.createElement('div');
         cardBody.className = "card-body";
         let cardTitle = document.createElement('h5');
@@ -37,6 +40,7 @@ fetch(`https://isko88.github.io/apipizza.json`)
         cardBody.appendChild(cardTitle);
         cardBody.appendChild(cardText);
         cardBody.appendChild(cardPrice);
+        cardBody.appendChild(button);
         card.appendChild(cardBody);
         col.appendChild(card);
         productsList.append(col);
@@ -44,184 +48,8 @@ fetch(`https://isko88.github.io/apipizza.json`)
     });
 })
 
-// posts.forEach(post => {
-//     let postTitle = document.createElement('h3');
-//     let postContent = document.createElement('p');
-//     postTitle.textContent = post.title;
-//     postContent.textContent = post.body;
-//     postsList.append(postTitle);
-//     postsList.append(postContent);
-// });
-// .then(user => {
-//     const name = document.getElementById("name");
-//     const email = document.getElementById("email");
-//     const image = document.getElementById("image");
-//     name.textContent = `${user.name.first} ${user.name.last}`;
-//     email.textContent = user.email;
-//     // created image
-//     var userImage = new Image();
-//     userImage.style.borderRadius = "50%";
-//     userImage.src = user.picture.medium;
-//     image.append(userImage);
-//     console.log(user)
+$('.basket-button').on('click', function() {
+    if ($('.basket').css('opacity') == 0) $('.basket').css('opacity', 1);
+    else $('.basket').css('opacity', 0);
+});
 
-//     // click
-//     name.onclick = function () {
-//         apiUsersList.innerHTML = "";
-//             let apiUserTr = document.createElement('tr');
-//             let userFullname = document.querySelector('div.modal.api-users-list .modal-title');
-//             let userEmail = document.createElement('td');
-//             let userGender = document.createElement('td');
-//             let userAge = document.createElement('td');
-//             let userPhone = document.createElement('td');
-//             let userCountry = document.createElement('td');
-//             let userCity = document.createElement('td');
-//             let userStreet = document.createElement('td');
-//             let userPostcode = document.createElement('td');
-
-            
-//             name.setAttribute("data-toggle", "modal");
-//             name.setAttribute("data-target", "#myModalUser");
-
-//             userFullname.textContent = `${user.name.first} ${user.name.last}`;
-//             userEmail.textContent = user.email;
-//             userGender.textContent = user.gender;
-//             userAge.textContent = user.dob.age;
-//             userPhone.textContent = user.phone;
-//             userCountry.textContent = user.location.country;
-//             userCity.textContent = user.location.city;
-//             userStreet.textContent = user.location.street.name;
-//             userPostcode.textContent = user.location.postcode;
-
-//                 apiUserTr.appendChild(userFullname);
-//                 apiUserTr.appendChild(userEmail);
-//                 apiUserTr.appendChild(userGender);
-//                 apiUserTr.appendChild(userAge);
-//                 apiUserTr.appendChild(userPhone);
-//                 apiUserTr.appendChild(userCountry);
-//                 apiUserTr.appendChild(userCity);
-//                 apiUserTr.appendChild(userStreet);
-//                 apiUserTr.appendChild(userPostcode);
-
-
-//                 apiUsersList.append(apiUserTr);
-        
-//     } 
-// })
-
-// [
-//     {
-//       "id": 1,
-//       "category": "Pizza",
-//       "name": "Quattro formaggi",
-//       "image":"https://isko88.github.io/pizza/1.png",
-//       "topping": [
-//         "Dough",
-//         "Mozzarella",
-//         "Cheddar",
-//         "Blue",
-//         "Parmesan"
-//       ],
-//       "price": 79
-//     },
-//     {
-//       "id": 2,
-//       "category": "Pizza",
-//       "name": "Carbonara",
-//       "image":"https://isko88.github.io/pizza/2.png",
-//       "topping": [
-//         "Dough",
-//         "Mozzarella",
-//         "Cheddar",
-//         "Blue",
-//         "Parmesan"
-//       ],
-//       "price": 79
-//     },
-//     {
-//       "id": 3,
-//       "category": "Pizza",
-//       "name": "Philadelphia salmone",
-//       "image":"https://isko88.github.io/pizza/3.png",
-//       "topping": [
-//         "Dough",
-//         "Mozzarella",
-//         "Cheddar",
-//         "Blue",
-//         "Parmesan"
-//       ],
-//       "price": 89
-//     },
-//     {
-//       "id": 4,
-//       "category": "Pizza",
-//       "name": "Veggie",
-//       "image":"https://isko88.github.io/pizza/4.png",
-//       "topping": [
-//         "Dough",
-//         "Mozzarella",
-//         "Cheddar",
-//         "Blue",
-//         "Parmesan"
-//       ],
-//       "price": 10
-//     },
-//     {
-//       "id": 5,
-//       "category": "Pizza",
-//       "name": "Margherita",
-//       "image":"https://isko88.github.io/pizza/5.png",
-//       "topping": [
-//         "Dough",
-//         "Mozzarella",
-//         "Cheddar",
-//         "Blue",
-//         "Parmesan"
-//       ],
-//       "price": 10
-//     },
-//     {
-//       "id": 6,
-//       "category": "Pizza",
-//       "name": "Pizzasallad",
-//       "image":"https://isko88.github.io/pizza/6.png",
-//       "topping": [
-//         "Dough",
-//         "Mozzarella",
-//         "Cheddar",
-//         "Blue",
-//         "Parmesan"
-//       ],
-//       "price": 0
-//     },
-//     {
-//       "id": 7,
-//       "category": "Pizza",
-//       "name": "Hawaiian",
-//       "image":"https://isko88.github.io/pizza/7.png",
-//       "topping": [
-//         "Dough",
-//         "Mozzarella",
-//         "Cheddar",
-//         "Blue",
-//         "Parmesan"
-//       ],
-//       "price": 10
-//     },
-//      {
-//       "id": 8,
-//       "category": "Pizza",
-//       "name": "Pepperoni",
-//       "image":"https://isko88.github.io/pizza/8.png",
-//       "topping": [
-//         "Dough",
-//         "Mozzarella",
-//         "Cheddar",
-//         "Blue",
-//         "Parmesan"
-//       ],
-//       "price": 10
-//     }
-//   ]
-
-  
